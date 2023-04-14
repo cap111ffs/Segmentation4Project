a0d4f7073bc7dd992eb28e9f2aea7e6afa4a9f5b
cap111ffs <mamed.bayramov.04.04@mail.ru>
2023-04-14 19:18:02 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from predict import predictColoredMask
+from PIL import Image, ImageFilter
+from pathlib import Path
+import tensorflow as tf
+
+
+input_path = '/Python/Segmentation/input/input.jpg'
+
+colored_mask_array = predictColoredMask(input_path)
+colored_mask = tf.keras.utils.array_to_img(colored_mask_array)
+
+with Image.open(input_path) as man_photo:
+    man_photo.load()
+
+mask = Image.new("L", colored_mask.size, 128)
+
+colored_mask.filter(ImageFilter.BoxBlur(20))
+
+segmented_photo = Image.composite(man_photo, colored_mask, mask)
+segmented_photo.save('/Python/Segmentation/output/ouput.jpg')
